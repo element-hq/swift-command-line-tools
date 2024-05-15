b30768277abb107f5d514489cae0145ddaac9334
Doug <6060466+pixlwave@users.noreply.github.com>
2024-05-15 14:58:40 +0100

    Add a basic git tool (using the git cli). (#1)

---
+++ Sources/CommandLineTools/Release/Git.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+/// A local git repo.
+public struct Git {
+    enum Error: Swift.Error {
+        case noOutput
+    }
+    
+    /// The directory of the repo.
+    public let directory: URL
+    
+    public init(directory: URL) {
+        self.directory = directory
+    }
+    
+    /// The name of the current branch.
+    public var branchName: String {
+        get throws {
+            guard let output = try Zsh.run(command: "git rev-parse --abbrev-ref HEAD", directory: directory) else { throw Error.noOutput }
+            return output.trimmingCharacters(in: .whitespacesAndNewlines)
+        }
+    }
+    
+    /// The hash of the current commit.
+    public var commitHash: String {
+        get throws {
+            guard let output = try Zsh.run(command: "git rev-parse HEAD", directory: directory) else { throw Error.noOutput }
+            return output.trimmingCharacters(in: .whitespacesAndNewlines)
+        }
+    }
+    
+    /// Add file contents to the index.
+    public func add(files: String...) throws {
+        try Zsh.run(command: "git add \(files.joined(separator: " "))", directory: directory)
+    }
+    
+    /// Record changes to the repository.
+    public func commit(message: String) throws {
+        try Zsh.run(command: "git commit -m \"\(message)\"", directory: directory)
+    }
+    
+    /// Update remote refs along with associated objects.
+    public func push() throws {
+        try Zsh.run(command: "git push", directory: directory)
+    }
+}
